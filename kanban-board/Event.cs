72989157c8a9be42e51c7496e0baa23c2eafab5c
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-11-06 14:05:27 +0000

    added the Event Class - Derivative of the Item class

---
+++ kanban-board/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanban_board
+{
+    internal class Event
+    {
+        private DateTime startDate
+        {
+            get { return this.startDate; }
+            set { this.startDate = value; }
+        }
+        private DateTime endDate
+        {
+            get { return this.endDate; }
+            set { this.endDate = value; }
+        }
+        public Event(DateTime sd, DateTime ed)
+        {
+            this.startDate = sd;
+            this.endDate = ed;
+        }
+    }
+}
